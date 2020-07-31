00100bc0878547bfb221fe11d1fb751899dc5f20
Fakhriddin Abdurakhimov <zealotrahl@gmail.com>
2020-08-01 00:52:49 +0500

    Feedback answer back

--- src/feedbacks/feedbacks.module.ts
+++ src/feedbacks/feedbacks.module.ts
@@ -3,10 +3,11 @@ import { FeedbacksService } from './feedbacks.service';
 import { FeedbacksController } from './feedbacks.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Feedback } from './feedback.entity';
+import { isFeedbackExists } from './validators';
 
 @Module({
   imports: [SequelizeModule.forFeature([Feedback])],
-  providers: [FeedbacksService],
+  providers: [FeedbacksService, isFeedbackExists],
   controllers: [FeedbacksController]
 })
 export class FeedbacksModule {}
