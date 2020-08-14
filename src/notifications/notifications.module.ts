6560b1327a6f0c9b2eca81b459a67e88acff781e
unknown <hubzein@gmail.com>
2020-08-14 21:58:01 +0500

    fi

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { NotificationsController } from './notifications.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Notification } from './notification.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Notification])],
   providers: [NotificationsService],
   controllers: [NotificationsController]
 })
