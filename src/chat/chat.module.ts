181fe66dfceef287efdbc975ec8e66dcca1772f3
unknown <hubzein@gmail.com>
2020-08-24 14:13:32 +0500

    fix imports

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -3,9 +3,13 @@ import { MessagesController } from "./messages.controller";
 import { MessagesService } from "./messages.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Message } from "./message.entity";
+import { GatewaysModule } from "src/gateways/gateways.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Message])],
+  imports: [
+    TypeOrmModule.forFeature([Message]),
+    GatewaysModule,
+  ],
   controllers: [MessagesController],
   providers: [MessagesService],
   exports: [MessagesService]
